4a031d30da49909ae4ead188849661258fcd22aa
Olena <alchimeraa@gmail.com>
2024-10-27 22:17:24 +0200

    components

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,21 +1,3 @@
-// import FriendListItem from "../FriendList/FriendList";
-// import styles from "../FriendList/FriendList.module.css";
-
-// const FriendList = ({ friends }) => (
-//   <ul className={styles.friendList}>
-//     {friends.map(friend => (
-//       <li key={friend.id} className={styles.friendListItem}>
-//         <FriendListItem
-//           avatar={friend.avatar}
-//           name={friend.name}
-//           isOnline={friend.isOnline}
-//         />
-//       </li>
-//     ))}
-//   </ul>
-// );
-
-// export default FriendList;
 
 // import React from "react";
 import FriendListItem from "../FriendListItem/FriendListItem";
